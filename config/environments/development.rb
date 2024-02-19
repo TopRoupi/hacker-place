484a412dbbbdaa0a6c885e84b047047a2b46e7a6
Breno Nunes <breno.nunesgalvao@protonmail.ch>
2024-02-19 09:28:11 +0000

    add gitpod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   config.action_controller.default_url_options = {host: "localhost", port: 3000}
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 
+  config.hosts << /.*(gitpod\.io\Z)/
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
